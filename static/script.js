201fb6fc6bf39d2a52da4b5ee4cb8bcba1287cf3
TIAAAAA <rauzino.mattia@iisgalvanimi.edu.it>
2026-04-20 08:51:59 +0000

    modifiche con post

--- static/script.js
+++ static/script.js
@@ -3,18 +3,29 @@ async function loggati() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    if (!username || !password) 
+    if (!username || !password)
         return alert("Scrivi lo username e password");
 
-    const res = await fetch(`/login?username=${username}&password=${password}`);
-    if (res.redirected) {
-        window.location.href = res.url;
-    } else {
-        const json = await res.json();
-        document.getElementById("risultato").innerText = json.messaggio;
-    }
+    const res = await fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `username=${username}&password=${password}`
+    });
 
+    const json = await res.json();
+
+    if (json.messaggio == 1){
+        document.getElementById("risultato").innerText = "Accesso effettuato";
+    }
+    else {
+        document.getElementById("risultato").innerText = "Accesso negato";
+    }
 }
+
+
+
 
 document.getElementById('bottone').addEventListener('click', loggati);
 
